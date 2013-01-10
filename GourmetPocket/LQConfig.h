6f720ecfdb8d4f567ee3a0cf3b1f26c566e99cd7
Bobie <bobie@herxun.co>
2013-01-10 17:39:51 +0800

    [what] create GourmetPocket repository on bobie-herxun github

---
+++ GourmetPocket/LQConfig.h
@@ -0,0 +1,8 @@
+
+// Create an application on the Geoloqi developers site and
+// enter your API Key here.
+// https://developers.geoloqi.com/
+
+// API key of GourmetPocket
+static NSString *const LQ_APIKey =      @"d4d4cf6e86ee7b63f1d214ac60e945bb";
+static NSString *const LQ_APISecret =   @"";
