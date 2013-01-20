fbe6ffb5b9efda766f439c2320272de6a6340fb5
BobieAir <ahchic@hotmail.com>
2013-01-20 23:04:11 +0800

    [what] add an API wrapper manager for Geoloqi places

---
+++ GeoloqiPlaceManager.h
@@ -0,0 +1,13 @@
+//
+//  GeoloqiPlaceManager.h
+//  GourmetPocket
+//
+//  Created by BobieAir on 13/1/19.
+//  Copyright (c) 2013年 Bobie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GeoloqiPlaceManager : NSObject
+
+@end
---
+++ GeoloqiPlaceManager.m
@@ -0,0 +1,13 @@
+//
+//  GeoloqiPlaceManager.m
+//  GourmetPocket
+//
+//  Created by BobieAir on 13/1/19.
+//  Copyright (c) 2013年 Bobie. All rights reserved.
+//
+
+#import "GeoloqiPlaceManager.h"
+
+@implementation GeoloqiPlaceManager
+
+@end
