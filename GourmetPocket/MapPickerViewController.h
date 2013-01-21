405a9bfeb377605819716286294b431910806dce
BobieAir <ahchic@hotmail.com>
2013-01-22 00:49:44 +0800

    [what] add new files 1. a "positioning" icon 2. the view-controller class for place picker map view

---
+++ GourmetPocket/MapPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapPickerViewController.h
+//  GourmetPocket
+//
+//  Created by BobieAir on 13/1/21.
+//  Copyright (c) 2013年 Bobie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapPickerViewController : UIViewController
+
+@end
