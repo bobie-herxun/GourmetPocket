dd1fa8722343ddbce15216e01a1903aae25a9824
BobieAir <ahchic@hotmail.com>
2013-01-28 00:03:28 +0800

    [what] adding new files view-controller for place details add-new-pic image

---
+++ GourmetPocket/PlaceDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaceDetailViewController.h
+//  GourmetPocket
+//
+//  Created by BobieAir on 13/1/26.
+//  Copyright (c) 2013年 Bobie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaceDetailViewController : UIViewController
+
+@end
---
+++ GourmetPocket/PlaceDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  PlaceDetailViewController.m
+//  GourmetPocket
+//
+//  Created by BobieAir on 13/1/26.
+//  Copyright (c) 2013年 Bobie. All rights reserved.
+//
+
+#import "PlaceDetailViewController.h"
+
+@interface PlaceDetailViewController ()
+
+@end
+
+@implementation PlaceDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
